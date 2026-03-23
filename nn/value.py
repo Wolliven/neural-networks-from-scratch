0382385973dbe5f34208207dc102d93d341ed6ad
Jerico San Nicolas <jericosnicolas@gmail.com>
2026-03-23 10:12:25 +0900

    feat: add Value multiplication

--- nn/value.py
+++ nn/value.py
@@ -4,10 +4,24 @@ import matplotlib.pyplot as plt
 
 class Value():
 
-    def __init__(self, data):
+    def __init__(self, data, _children=(), _op=''):
         self.data = data
+        self._prev = set(_children)
+        self._op = _op
 
     def __repr__(self):
         return f"Value(data={self.data})"
 
-print(Value(2.0))
+    def __add__(self, other):
+        return Value(self.data + other.data, (self, other), '+')
+    
+    def __mul__(self, other):
+        return Value(self.data*other.data, (self, other), '*')
+
+
+a = Value(2.0)
+b = Value(-6.0)
+c = a*b
+d = Value(5.0)
+e = d+c
+print(e._prev)
