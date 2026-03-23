5f1dbc457b9332a9fdc329591b944a259c5eb7fc
Jerico San Nicolas <jericosnicolas@gmail.com>
2026-03-23 11:16:07 +0900

    feat: add graph visualization

--- nn/value.py
+++ nn/value.py
@@ -1,27 +1,67 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from graphviz import Digraph
+
 
 class Value():
 
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self._prev = set(_children)
         self._op = _op
+        self.label = label
 
     def __repr__(self):
         return f"Value(data={self.data})"
 
     def __add__(self, other):
-        return Value(self.data + other.data, (self, other), '+')
+        out = Value(self.data + other.data, (self, other), '+')
+        return out
     
     def __mul__(self, other):
-        return Value(self.data*other.data, (self, other), '*')
+        out = Value(self.data*other.data, (self, other), '*')
+        return out
 
 
-a = Value(2.0)
-b = Value(-6.0)
-c = a*b
-d = Value(5.0)
-e = d+c
-print(e._prev)
+a = Value(2.0, label='a')
+b = Value(-6.0, label='b')
+c = a*b; c.label = 'c'
+d = Value(5.0, label='d')
+L = d+c; L.label = 'L'
+
+
+def trace(root):
+    nodes, edges = set(), set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child, v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def draw_dot(root):
+    dot = Digraph(format='svg', graph_attr={'rankdir': 'LR'}) # LR = left to right
+
+    nodes, edges = trace(root)
+    for n in nodes:
+        uid = str(id(n))
+        # for any value in the graph, create a rectangular ('record') node for it
+        dot.node(name = uid, label = "{ %s | data %.4f}" % (n.label, n.data), shape='record')
+        if n._op:
+        # if this value is a result of some operation, create an op node for it
+            dot.node(name = uid + n._op, label = n._op)
+        # and connect this node to it
+        if n._op:
+            dot.edge(uid + n._op, uid)
+
+    for n1, n2 in edges:
+        # connect n1 to the op node of n2
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+
+    return dot
+
+dot = draw_dot(L)
+dot.render("graph.dot", view=True)
